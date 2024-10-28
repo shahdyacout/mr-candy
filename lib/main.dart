13c5e0cf9b828cc77817a5c92dee281e7e63b460
shahd yacout <shahdyacout4@gmail.com>
2024-10-28 20:47:27 +0200

    feature/login branch created

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(MrCandy());
+}
+class MrCandy extends StatelessWidget {
+  const MrCandy({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ,
+    );
+  }
+}
