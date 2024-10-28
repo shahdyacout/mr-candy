fead40c17c4c7a3db5ab0a6432790397e0242674
shahd yacout <shahdyacout4@gmail.com>
2024-10-28 20:47:27 +0200

    login repo is done.

---
+++ lib/features/login/data/models/user_model.dart
@@ -0,0 +1,26 @@
+class UserModel{
+
+
+final int id;
+final String name;
+final String email;
+final String phone;
+final String image;
+final int points;
+final int credit;
+final String token;
+
+UserModel({
+  required this.id,
+  required this.name,
+  required this.email,
+  required this.phone,
+  required this.image,
+  required this.points,
+  required this.credit,
+  required this.token
+});
+
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,9 @@ class MrCandy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ,
+
+      home:Scaffold() ,
+
     );
   }
 }
